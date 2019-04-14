3e59abcdbd062c9d3326fb30605e503091037943
shashikiran007 <49611982+shashikiran007@users.noreply.github.com>
2019-04-14 21:23:49 +0530

    first page

---
+++ action.js
@@ -0,0 +1,14 @@
+function action () {
+	console.log("Shoot");
+}
+
+action();
+
+
+var fbObj = {
+	username: "darkmatters",
+	password: "white",
+}
+
+
+var database = ["orca", "great white sharks", "dolphins"]
